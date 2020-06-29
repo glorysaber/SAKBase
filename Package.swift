6ddf522d4059cb3cfd69112a71609a93f3a5da55
Stephen Kac <Stephen@StephenKac.com>
2020-06-29 14:03:12 -0600

    Updated project structure for Swift PM

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SAKBase",
-		platforms: [.macOS(.v10_12),
-								.iOS(.v10),
-								.tvOS(.v10),
-								.watchOS(.v3)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -24,12 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SAKBase",
-            dependencies: [],
-						path: "Sources"),
+            dependencies: []),
         .testTarget(
             name: "SAKBaseTests",
-            dependencies: ["SAKBase"],
-						path: "SAKBaseTests"),
-    ],
-		swiftLanguageVersions: [.v5]
+            dependencies: ["SAKBase"]),
+    ]
 )
