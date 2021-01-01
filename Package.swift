cd709a7988a96fb5f6d0687973abb30aa21a1823
Stephen Kac <stephen@stephenkac.com>
2021-01-01 12:52:18 -0600

    Minimum macOS target is now 10.15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
 	name: "SAKBase",
-	platforms: [.macOS(.v10_14),
+	platforms: [.macOS(.v10_15),
 							.iOS(.v13),
 							.tvOS(.v13),
 							.watchOS(.v6)
